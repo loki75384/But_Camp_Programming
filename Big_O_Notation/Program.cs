de9dfbf6c8d35131eb99e42839c2cfa9512395f8
loki47 <larsentuta49@gmail.com>
2023-08-01 12:16:18 +0300

    create file

---
+++ Big_O_Notation/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(array[3]);
+// Сложность алгоритма O(1)
