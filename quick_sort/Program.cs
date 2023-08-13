1d1b9844db0d4c06a97d513862259053ddfffcc8
loki47 <larsentuta49@gmail.com>
2023-08-13 16:25:32 +0300

    быстрая сортировка

--- quick_sort/Program.cs
+++ quick_sort/Program.cs
@@ -6,8 +6,48 @@
     else 
     {
         int pivot = arr[0];
-        int[] left = arr.Skip(1).Where(x => x < pivot).ToArray();
-        int[] right = arr.Skip(1).Where(x => x >= pivot).ToArray();
+        int count_left = 0;
+        int count_right = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                count_left++;
+            }
+            else
+            {
+                count_right++;
+            }
+        }
+        
+        int[] left = new int[count_left];
+        int[] right = new  int[count_right];
+        int num_left = 0;
+        int num_right = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                left[num_left] = arr[i];
+                num_left++;
+            }
+            else
+            {
+                right[num_right] = arr[i];
+                num_right++;
+            }
+        }
+
+        // int[] left = arr.Skip(1).Where(x => x < pivot).ToArray();
+        // int[] right = arr.Skip(1).Where(x => x >= pivot).ToArray();
+        
+        
+        // int result_size = left.Length + 1 + right.Length;
+        // int[] result_arr = new int [result_size]
+        // for (int i = 0; i < result_size; i++)
+        // {
+            
+        // }
         return QuickSort(left).Concat(new int[] {pivot}).Concat(QuickSort(right)).ToArray();
     }
 }
